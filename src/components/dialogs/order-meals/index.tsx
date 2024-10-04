5c8cec2afc243b443a1a53854efe664902f15a82
Mrinal Singh <mrinalsingh@CRDL-LAP-0045.local>
2024-10-04 18:50:26 +0530

    order meals ui updated

--- src/components/dialogs/order-meals/index.tsx
+++ src/components/dialogs/order-meals/index.tsx
@@ -4,8 +4,8 @@
 import { useEffect, useState } from 'react'
 
 // MUI Imports
-import CustomIconButton from '@/@core/components/mui/IconButton'
 import { ProductDataType, TableDataType } from '@/types/adminTypes'
+import { CustomerListType } from '@/types/staffTypes'
 import { Autocomplete, Divider, Drawer, TextField, Typography } from '@mui/material'
 import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
@@ -21,6 +21,7 @@ type OrderMealsPropType = {
 }
 
 type MealCartType = {
+  customer: CustomerListType
   order: {
     product: ProductDataType
     quantity: number | string
@@ -32,6 +33,7 @@ type MealCartType = {
 const OrderMeals = ({ open, setOpen, tableData, getAllTablesData }: OrderMealsPropType) => {
   // States
   const [productList, setProductList] = useState([] as ProductDataType[])
+  const [fieldIndex, setFieldIndex] = useState(0)
   // const [inputData, setInputData] = useState({
   //   discount: '',
   //   paymentMethod: paymentMethods[0],
@@ -51,10 +53,14 @@ const OrderMeals = ({ open, setOpen, tableData, getAllTablesData }: OrderMealsPr
     reset: resetForm,
     handleSubmit,
     watch,
+    getValues,
+    setError,
+    clearErrors,
     formState: { errors }
   } = useForm<MealCartType>({
     //resolver: yupResolver(schema),
     defaultValues: {
+      customer: tableData?.gameData?.players?.[0],
       order: [
         {
           product: productList[0],
@@ -69,12 +75,41 @@ const OrderMeals = ({ open, setOpen, tableData, getAllTablesData }: OrderMealsPr
     name: 'order' // unique name for your Field Array
   })
 
-  let subTotal = 0
-  watch('order').map(input => {
-    if (input.product?.salePrice && input.quantity) {
-      subTotal = subTotal + Number(input.product.salePrice) * Number(input.quantity)
+  const handleAddItem = () => {
+    if (!watch(`order.${fieldIndex}.product`)) {
+      setError(`order.${fieldIndex}.product`, { type: 'required', message: 'This field is required' })
+    } else if (!watch(`order.${fieldIndex}.quantity`)) {
+      setError(`order.${fieldIndex}.quantity`, { type: 'required', message: 'This field is required' })
+    } else {
+      clearErrors('order')
+      setFieldIndex(fields.length)
+      append({ product: productList[0], quantity: '' })
     }
-  })
+  }
+
+  const removeItem = (index: number) => {
+    remove(index)
+    setFieldIndex(fieldIndex - 1)
+  }
+
+  const handleProductChange = (value: ProductDataType | null, onChange: (value: ProductDataType | null) => void) => {
+    clearErrors(`order.${fieldIndex}.product`)
+    onChange(value)
+  }
+
+  const handleQuantityChange = (value: string | null, onChange: (value: string | null) => void) => {
+    clearErrors(`order.${fieldIndex}.quantity`)
+    onChange(value)
+  }
+
+  let subTotal = 0
+  getValues('order')
+    .slice(0, -1)
+    .map(input => {
+      if (input.product?.salePrice && input.quantity) {
+        subTotal = subTotal + Number(input.product.salePrice) * Number(input.quantity)
+      }
+    })
   const tax = ((subTotal * 10) / 100).toFixed(2)
   const total = (Number(subTotal) + Number(tax)).toFixed(2)
   // const cashOut = (Number(inputData.cashIn ?? 0) - Number(total ?? 0)).toFixed(2)
@@ -118,6 +153,11 @@ const OrderMeals = ({ open, setOpen, tableData, getAllTablesData }: OrderMealsPr
   }, [])
 
   const onSubmit = async (data: MealCartType) => {
+    data.order = data.order.slice(0, -1)
+    if (!data.order.length) {
+      toast.error('Please add items to place order')
+      return
+    }
     const orderList = data.order.map(ord => {
       return {
         productId: ord.product._id,
@@ -170,72 +210,144 @@ const OrderMeals = ({ open, setOpen, tableData, getAllTablesData }: OrderMealsPr
       <div className='p-5'>
         <form onSubmit={handleSubmit(data => onSubmit(data))}>
           <div className='flex flex-col gap-5'>
-            {fields.map((field, index) => (
-              <div key={field.id} className='flex flex-col sm:flex-row items-start mbe-4 gap-3'>
-                <Controller
-                  name={`order.${index}.product`}
-                  control={control}
-                  rules={{ required: true }}
-                  render={({ field: { value, onChange } }) => (
-                    <Autocomplete
-                      fullWidth
-                      options={productList}
-                      getOptionLabel={option => `${option.productName} (₹${option.salePrice})`}
-                      value={value}
-                      isOptionEqualToValue={(option, value) => option._id === value._id}
-                      onChange={(_, value) => onChange(value)}
-                      renderInput={params => (
-                        <TextField
-                          {...params}
-                          variant='outlined'
-                          label='Product'
-                          {...(errors.order?.[index]?.product && {
-                            error: true,
-                            helperText: errors.order?.[index]?.product?.message || 'This field is required'
-                          })}
-                        />
-                      )}
-                    />
-                  )}
-                />
+            {tableData?.gameData?.players ? (
+              <Controller
+                name='customer'
+                control={control}
+                rules={{ required: true }}
+                render={({ field: { value, onChange } }) => (
+                  <Autocomplete
+                    fullWidth
+                    options={tableData?.gameData?.players ?? []}
+                    getOptionLabel={option => option.fullName?.split('(').join(' (')}
+                    value={value}
+                    isOptionEqualToValue={(option, value) => option.customerId === value.customerId}
+                    onChange={(_, value) => onChange(value)}
+                    renderInput={params => (
+                      <TextField
+                        {...params}
+                        variant='outlined'
+                        label='Customer'
+                        {...(errors.customer && {
+                          error: true,
+                          helperText: errors.customer.message || 'This field is required'
+                        })}
+                      />
+                    )}
+                  />
+                )}
+              />
+            ) : (
+              <></>
+            )}
 
-                <Controller
-                  name={`order.${index}.quantity`}
-                  control={control}
-                  rules={{ required: true }}
-                  render={({ field: { value, onChange } }) => (
-                    <TextField
-                      fullWidth
-                      label='Quantity'
-                      inputProps={{ type: 'number', min: 0 }}
-                      value={value}
-                      onChange={onChange}
-                      {...(errors.order?.[index]?.quantity && {
-                        error: true,
-                        helperText: errors.order?.[index]?.quantity?.message || 'This field is required'
-                      })}
-                    />
-                  )}
-                />
+            <div key={fields[fieldIndex].id} className='flex flex-col sm:flex-row items-start gap-3'>
+              <Controller
+                name={`order.${fieldIndex}.product`}
+                control={control}
+                render={({ field: { value, onChange } }) => (
+                  <Autocomplete
+                    fullWidth
+                    options={productList}
+                    getOptionLabel={option => `${option.productName} (₹${option.salePrice})`}
+                    value={value}
+                    isOptionEqualToValue={(option, value) => option._id === value._id}
+                    onChange={(_, value) => handleProductChange(value, onChange)}
+                    renderInput={params => (
+                      <TextField
+                        {...params}
+                        variant='outlined'
+                        label='Product'
+                        {...(errors.order?.[fieldIndex]?.product && {
+                          error: true,
+                          helperText: errors.order?.[fieldIndex]?.product?.message || 'This field is required'
+                        })}
+                      />
+                    )}
+                  />
+                )}
+              />
 
-                {fields.length > 1 ? (
+              <Controller
+                name={`order.${fieldIndex}.quantity`}
+                control={control}
+                render={({ field: { value, onChange } }) => (
+                  <TextField
+                    fullWidth
+                    label='Quantity'
+                    inputProps={{ type: 'number', min: 0 }}
+                    value={value}
+                    onChange={event => handleQuantityChange(event.target.value, onChange)}
+                    {...(errors.order?.[fieldIndex]?.quantity && {
+                      error: true,
+                      helperText: errors.order?.[fieldIndex]?.quantity?.message || 'This field is required'
+                    })}
+                  />
+                )}
+              />
+
+              {/* {fields.length > 1 ? (
                   <CustomIconButton onClick={() => remove(index)} className='min-is-fit'>
                     <i className='ri-close-line' />
                   </CustomIconButton>
                 ) : (
                   <></>
-                )}
+                )} */}
+            </div>
+
+            <div className='flex justify-end'>
+              <Button
+                className='min-is-fit'
+                size='small'
+                variant='contained'
+                onClick={handleAddItem}
+                startIcon={<i className='ri-add-line' />}
+              >
+                Add Item
+              </Button>
+            </div>
+
+            {fields.slice(0, -1).length ? (
+              <div className='w-full grid grid-cols-1 border mt-2 rounded-lg overflow-x-auto '>
+                <div className='w-full text-center font-bold border-b p-1 sm:p-2'>Ordered Items</div>
+                <div className='w-full grid grid-cols-4 text-center font-bold border-b divide-x'>
+                  <div className='size-full grid place-items-center p-1 sm:p-2 '>
+                    <p>Item</p>
+                  </div>
+                  <div className='size-full grid place-items-center p-1 sm:p-2'>
+                    <p>Qty</p>
+                  </div>
+                  <div className='size-full grid place-items-center p-1 sm:p-2'>
+                    <p>Amount</p>
+                  </div>
+                  <div className='size-full grid place-items-center p-1 sm:p-2'>
+                    <p>Action</p>
+                  </div>
+                </div>
+
+                {fields.slice(0, -1).map((orderItem, index) => (
+                  <div
+                    key={fields[index].id}
+                    className={`w-full grid grid-cols-4 divide-x ${fields.slice(0, -1).length - 1 !== index ? 'border-b' : ''}`}
+                  >
+                    <div className='size-full grid place-items-center break-all p-1 sm:p-2'>
+                      <p>{orderItem.product?.productName}</p>
+                    </div>
+                    <div className='size-full grid place-items-center p-1 sm:p-2'>
+                      <p>{orderItem.quantity}</p>
+                    </div>
+                    <div className='size-full grid place-items-center p-1 sm:p-2'>
+                      <p>{`₹${orderItem.product?.salePrice ?? 0}`}</p>
+                    </div>
+                    <div className='size-full grid place-items-center p-1 sm:p-2'>
+                      <i className='ri-close-line' onClick={() => removeItem(index)} />
+                    </div>
+                  </div>
+                ))}
               </div>
-            ))}
-            <Button
-              className='min-is-fit'
-              size='small'
-              variant='contained'
-              onClick={() => append({ product: productList[0], quantity: '' })}
-              startIcon={<i className='ri-add-line' />}
-            >
-              Add Item
-            </Button>
+            ) : (
+              <></>
+            )}
 
             <div className='w-full grid grid-cols-3 gap-2 border p-4 mt-2 rounded-lg'>
               <p className='col-span-2'>Sub Total</p>
