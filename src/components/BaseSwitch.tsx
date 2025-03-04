64da7dad10184262c7de39d1dbcaecb850566397
Mrinal Singh <mrinalsingh@CRDL-LAP-0045.local>
2025-03-05 00:41:04 +0530

    ui fix

--- src/components/BaseSwitch.tsx
+++ src/components/BaseSwitch.tsx
@@ -15,7 +15,7 @@ const BaseSwitch = ({ value: initialValue, onChange }: { value: boolean; onChang
 
   return (
     <label
-      className='flex items-center relative cursor-pointer select-none w-20 h-7'
+      className='flex items-center relative cursor-pointer select-none w-[70px] h-5'
       style={{ containerType: 'inline-size' }}
     >
       <input
@@ -24,16 +24,16 @@ const BaseSwitch = ({ value: initialValue, onChange }: { value: boolean; onChang
         onChange={handleChange}
         className='appearance-none transition-colors cursor-pointer size-full rounded-full bg-red-500 checked:bg-green-500 peer'
       />
-      <span className='absolute font-medium text-[9px] uppercase right-0 text-white peer-checked:hidden mr-2'>
+      <span className='absolute font-medium text-[9px] uppercase right-0 text-white peer-checked:hidden mr-1'>
         DISABLED
       </span>
-      <span className='absolute font-medium text-[9px] uppercase left-0 text-white peer-checked:block hidden ml-2'>
+      <span className='absolute font-medium text-[9px] uppercase left-0 text-white peer-checked:block hidden ml-1'>
         ENABLED
       </span>
       <div
-        className={`w-7 h-7 absolute rounded-full transform transition-transform bg-gray-950/60 grid place-items-center peer-checked:translate-x-[calc(100cqw-100%)] peer-checked:text-green-500 text-red-500`}
+        className={`w-5 h-5 absolute rounded-full transform transition-transform bg-gray-950/60 grid place-items-center peer-checked:translate-x-[calc(100cqw-100%)] peer-checked:text-green-500 text-red-500 p-1`}
       >
-        <i className='ri-shut-down-line'></i>
+        <i className='ri-shut-down-line size-full'></i>
       </div>
     </label>
   )
